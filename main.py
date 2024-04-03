149241375611032f08af2737542e63f9a5677801
Luceta <57941164+Luceta@users.noreply.github.com>
2024-04-03 18:52:09 +0900

    7.5 Streamitpages, messages ui

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-import streamlit
+import streamlit as st
 
-streamlit.title("hello world")
+st.set_page_config(
+    page_title="FullstackGPT Home",
+    page_icon="🤖",
+)
+
+st.markdown(
+    """
+# Hello!
+            
+Welcome to my FullstackGPT Portfolio!
+            
+Here are the apps I made:
+            
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
+"""
+)
