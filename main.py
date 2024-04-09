71fc99c3c95195097a2d7a81650d653fd43edf9a
Luceta <57941164+Luceta@users.noreply.github.com>
2024-04-10 01:51:20 +0900

    deploy intial siteGPT

--- main.py
+++ main.py
@@ -1,23 +1,195 @@
+import os
+from typing import List, Dict, Any
+from langchain.docstore.document import Document
+from langchain_core import retrievers
+from bs4 import BeautifulSoup
+
+from langchain.document_loaders import SitemapLoader
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 import streamlit as st
 
-st.set_page_config(
-    page_title="FullstackGPT Home",
-    page_icon="🤖",
+# llm = ChatOpenAI(
+#    temperature=0.1,
+# )
+
+answers_prompt = ChatPromptTemplate.from_template(
+    """
+    Using ONLY the following context answer the user's question. If you can't just say you don't know, don't make anything up.
+                                                  
+    Then, give a score to the answer between 0 and 5.
+
+    If the answer answers the user question the score should be high, else it should be low.
+
+    Make sure to always include the answer's score even if it's 0.
+
+    Context: {context}
+                                                  
+    Examples:
+                                                  
+    Question: How far away is the moon?
+    Answer: The moon is 384,400 km away.
+    Score: 5
+                                                  
+    Question: How far away is the sun?
+    Answer: I don't know
+    Score: 0
+                                                  
+    Your turn!
+
+    Question: {question}
+"""
 )
+
+
+def get_answers(inputs: Dict[str, Any]) -> Dict:
+    docs: List[Document] = inputs["docs"]
+    question: str = inputs["question"]
+    answers_chain = answers_prompt | llm
+
+    return {
+        "question": question,
+        "answers": [
+            {
+                "answer": answers_chain.invoke(
+                    {"question": question, "context": doc.page_content}
+                ).content,
+                "source": doc.metadata["source"],
+                "date": doc.metadata["lastmod"],
+            }
+            for doc in docs
+        ],
+    }
+
+
+choose_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Use ONLY the following pre-existing answers to answer the user's question.
+
+            Use the answers that have the highest score (more helpful) and favor the most recent ones.
+
+            Cite sources and return the sources of the answers as they are, do not change them.
+
+            Answers: {answers}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+
+def choose_answer(inputs: Dict[str, Any]) -> Any:
+    answers = inputs["answers"]
+    question = inputs["question"]
+    choose_chain = choose_prompt | llm
+    condensed = "\n\n".join(
+        f"{answer['answer']}\nSource:{answer['source']}\nDate:{answer['date']}\n"
+        for answer in answers
+    )
+    return choose_chain.invoke(
+        {
+            "question": question,
+            "answers": condensed,
+        }
+    )
+
+
+def parse_page(soup: BeautifulSoup) -> str:
+    header = soup.find("header")
+    footer = soup.find("footer")
+    if header:
+        header.decompose()
+    if footer:
+        footer.decompose()
+    return (
+        str(soup.get_text())
+        .replace("\n", " ")
+        .replace("\xa0", " ")
+        .replace("CloseSearch Submit Blog", "")
+    )
+
+
+@st.cache_resource(show_spinner="Loading website...")
+def load_website(url: str) -> retrievers:
+    splitter: RecursiveCharacterTextSplitter = (
+        RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            chunk_size=1000,
+            chunk_overlap=200,
+        )
+    )
+    loader: SitemapLoader = SitemapLoader(
+        url,
+        filter_urls=[
+            r"^(.*\/blog\/).*",
+        ],
+        parsing_function=parse_page,
+    )
+    loader.requests_per_second = 2
+    docs: List[Document] = loader.load_and_split(text_splitter=splitter)
+    vector_store: FAISS = FAISS.from_documents(docs, OpenAIEmbeddings())
+    return vector_store.as_retriever()
+
+
+st.set_page_config(
+    page_title="SiteGPT",
+    page_icon="🖥️",
+)
+
 
 st.markdown(
     """
-# Hello!
+    # SiteGPT
             
-Welcome to my FullstackGPT Portfolio!
+    Ask questions about the content of a website.
             
-Here are the apps I made:
-            
-- [ ] [DocumentGPT](/documentGPT)
-- [ ] [PrivateGPT](/privateGPT)
-- [ ] [QuizGPT](/quizGPT)
-- [ ] [SiteGPT](/siteGPT)
-- [ ] [MeetingGPT](/meetingGPT)
-- [ ] [InvestorGPT](/investorGPT)
+    Start by writing the URL of the website on the sidebar.
 """
 )
+
+
+# OpenAI API 키 입력 및 설정
+with st.sidebar:
+
+    openai_api_key = st.text_input(
+        "OpenAI API Key", key="openai_api_key", type="password"
+    )
+    os.environ["OPENAI_API_KEY"] = openai_api_key
+    st.markdown(
+        "[GitHub Repository](https://github.com/Luceta/FullStack-gpt/tree/feat/quizGPT)"
+    )
+    url = st.text_input(
+        "Write down a URL",
+        placeholder="https://example.com",
+    )
+
+
+if url:
+    if ".xml" not in url:
+        with st.sidebar:
+            st.error("Please write down a Sitemap URL.")
+    else:
+
+        llm = ChatOpenAI(
+            temperature=0.1,
+        )
+
+        retriever = load_website(url)
+        query = st.text_input("Ask a question to the website.")
+        if query:
+            chain = (
+                {
+                    "docs": retriever,
+                    "question": RunnablePassthrough(),
+                }
+                | RunnableLambda(get_answers)
+                | RunnableLambda(choose_answer)
+            )
+            result = chain.invoke(query)
+            st.markdown(result.content.replace("$", "\$"))
