6bb3f8a5006c2b0203adca660883dfbc61230bcb
Luceta <57941164+Luceta@users.noreply.github.com>
2024-04-15 03:20:41 +0900

    fix: class funciton map

--- pages/openAIGPT.py
+++ pages/openAIGPT.py
@@ -105,69 +105,68 @@ functions = [
 ]
 
 
-class ThreadClient:
-    def __init__(self, client):
-        self.client = client
+def create_thread(content):
+    thread = client.beta.threads.create(
+        messages=[
+            {
+                "role": "user",
+                "content": content,
+            }
+        ]
+    )
+    return thread
 
-    def create_thread(self, content):
-        thread = self.client.beta.threads.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": content,
-                }
-            ]
+
+def get_run(run_id, thread_id):
+    return client.beta.threads.runs.retrieve(
+        run_id=run_id,
+        thread_id=thread_id,
+    )
+
+
+def send_message(thread_id, content):
+    return client.beta.threads.messages.create(
+        thread_id=thread_id, role="user", content=content
+    )
+
+
+def get_messages(thread_id):
+    messages = client.beta.threads.messages.list(thread_id=thread_id).data
+    messages.reverse()
+    for message in messages:
+        with st.chat_message(message.role):
+            if message.content:
+                st.markdown(message.content[0].text.value)
+                if hasattr(message.content[0], "text"):
+                    if (
+                        hasattr(message.content[0].text, "annotations")
+                        and message.content[0].text.annotations
+                    ):
+                        for annotation in message.content[0].text.annotations:
+                            st.markdown(f"Source: {annotation.ed}")
+
+
+def get_tool_outputs(run_id, thread_id):
+    run = get_run(run_id, thread_id)
+    outputs = []
+    for action in run.required_action.submit_tool_outputs.tool_calls:
+        action_id = action.id
+        function = action.function
+        print(f"Calling function: {function.name} with arg {function.arguments}")
+        outputs.append(
+            {
+                "output": functions_map[function.name](json.loads(function.arguments)),
+                "tool_call_id": action_id,
+            }
         )
-        return thread
+    return outputs
 
-    def get_run(self, run_id, thread_id):
-        return self.client.beta.threads.runs.retrieve(
-            run_id=run_id,
-            thread_id=thread_id,
-        )
 
-    def send_message(self, thread_id, content):
-        return self.client.beta.threads.messages.create(
-            thread_id=thread_id, role="user", content=content
-        )
-
-    def get_messages(self, thread_id):
-        messages = self.client.beta.threads.messages.list(thread_id=thread_id).data
-        messages.reverse()
-        for message in messages:
-            with st.chat_message(message.role):
-                if message.content:
-                    st.markdown(message.content[0].text.value)
-                    if hasattr(message.content[0], "text"):
-                        if (
-                            hasattr(message.content[0].text, "annotations")
-                            and message.content[0].text.annotations
-                        ):
-                            for annotation in message.content[0].text.annotations:
-                                st.markdown(f"Source: {annotation.ed}")
-
-    def get_tool_outputs(self, run_id, thread_id):
-        run = self.get_run(run_id, thread_id)
-        outputs = []
-        for action in run.required_action.submit_tool_outputs.tool_calls:
-            action_id = action.id
-            function = action.function
-            print(f"Calling function: {function.name} with arg {function.arguments}")
-            outputs.append(
-                {
-                    "output": self.functions_map[function.name](
-                        json.loads(function.arguments)
-                    ),
-                    "tool_call_id": action_id,
-                }
-            )
-        return outputs
-
-    def submit_tool_outputs(self, run_id, thread_id):
-        outputs = self.get_tool_outputs(run_id, thread_id)
-        return self.client.beta.threads.runs.submit_tool_outputs(
-            run_id=run_id, thread_id=thread_id, tool_outputs=outputs
-        )
+def submit_tool_outputs(run_id, thread_id):
+    outputs = get_tool_outputs(run_id, thread_id)
+    return client.beta.threads.runs.submit_tool_outputs(
+        run_id=run_id, thread_id=thread_id, tool_outputs=outputs
+    )
 
 
 def main():
@@ -201,7 +200,6 @@ def main():
         st.warning("Please enter your OpenAI API Key first!!")
     else:
         open_ai_client = client.Client(api_key=api_key)
-        thread_client = ThreadClient(open_ai_client)
 
         if assistant_id == "":
             assistant = open_ai_client.beta.assistants.create(
@@ -221,10 +219,10 @@ def main():
             st.write(f"Created a new assistant! ID: {assistant_id}")
 
     if run_id != "":
-        run = thread_client.get_run(run_id, thread_id)
+        run = get_run(run_id, thread_id)
         if run.status == "completed":
             st.success("The search is completed. The details are as follows")
-            thread_client.get_messages(thread_id)
+            get_messages(thread_id)
 
         elif run.status == "in_progress":
             with st.status("In progress..."):
@@ -233,7 +231,7 @@ def main():
                 st.rerun()
         elif run.status == "requires_action":
             with st.status("Processing action..."):
-                thread_client.submit_tool_outputs(run_id, thread_id)
+                submit_tool_outputs(run_id, thread_id)
                 time.sleep(3)
                 st.rerun()
 
@@ -251,7 +249,7 @@ def main():
             thread_id = thread.id
             st.session_state["thread_id"] = thread_id
         else:
-            thread_client.send_message(thread_id, keyword)
+            send_message(thread_id, keyword)
 
         run = open_ai_client.beta.threads.runs.create(
             thread_id=thread_id,
